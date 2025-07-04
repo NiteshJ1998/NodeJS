1924fad93a2d90c88a96e4c189abb7477b076e64
NiteshJ1998 <niteshjaiswar10@gmail.com>
2025-07-04 17:47:48 +0530

    REST API

--- 06-REST-API-Using-ExpressJS/index.js
+++ 06-REST-API-Using-ExpressJS/index.js
@@ -1,14 +1,44 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 2000;
 
-app.get("/", (req, res) => {
-  return res.send("Home Page");
+// Mobile Friendly response
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+      ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>
+  `;
+  res.send(html);
 });
 
-app.get("/about", (req, res) => {
-  return res.send("This is about page");
+//REST API
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
 });
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    // To get the id
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id); // find the id on mock data
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    //Edit User with id
+    res.json({ status: "Pending" });
+  })
+  .post((req, res) => {
+    res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    res.json({ status: "Pending" });
+  });
+
+// Patch means we have to edit some user
 
 app.listen(PORT, () => console.log(`Server Started at PORT${PORT} `));
