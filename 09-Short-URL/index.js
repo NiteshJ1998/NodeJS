46c053dadcf247de83c296840652fa1792bb97c5
NiteshJ1998 <niteshjaiswar10@gmail.com>
2025-07-11 09:39:45 +0530

    Short-URL

--- 09-Short-URL/index.js
+++ 09-Short-URL/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect.js");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter.js");
 const URL = require("./models/url");
 const app = express();
 const PORT = 8000;
@@ -9,9 +11,22 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("MongoDB connected")
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls: allUrls,
+  });
+});
 
 app.use("/url", urlRoute);
+
+app.use("/", staticRoute);
 
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
