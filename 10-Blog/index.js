53b40653f9070ae65d9ec295ea4ab20ab8157e46
NiteshJ1998 <niteshjaiswar10@gmail.com>
2025-07-20 02:10:04 +0530

    Blog

---
+++ 10-Blog/index.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userRoute = require("./routes/user");
+
+const app = express();
+const PORT = 8000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blogg")
+  .then((e) => console.log("mongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.use("/user", userRoute);
+
+app.listen(PORT, () => console.log(`Sever started at PORT: ${PORT}`));
---
+++ 10-Blog/models/user.js
@@ -0,0 +1,53 @@
+const { createHmac, randomBytes } = require("crypto");
+
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageURL: {
+      type: String,
+      default: "/images/user-profile-default.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+  const salt = randomBytes(16).toString("hex");
+  const hashPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashPassword;
+
+  next();
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
