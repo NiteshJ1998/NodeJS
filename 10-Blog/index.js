9341800c3eba7bda1cba1a7650a6aa45a9c0318c
NiteshJ1998 <niteshjaiswar10@gmail.com>
2025-07-21 16:36:52 +0530

    Blog

--- 10-Blog/index.js
+++ 10-Blog/index.js
@@ -1,12 +1,16 @@
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8000;
 
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }));
 mongoose
   .connect("mongodb://127.0.0.1:27017/blogg")
   .then((e) => console.log("mongoDB Connected"));
