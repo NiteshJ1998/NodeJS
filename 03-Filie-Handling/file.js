e386fc6700dc65cf0c731a7e8de2b1f6d4dcdbad
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2025-11-03 04:09:39 +0530

    added readFile fn

--- 03-Filie-Handling/file.js
+++ 03-Filie-Handling/file.js
@@ -1,14 +1,27 @@
-const fs = require("fs");
+const fs = require("fs"); // file system
 
-//Sync
-// fs.writeFileSync("./test.txt", "Hello there");
+//Sync... call
+// create file  pass data
+// 1 argument  2nd argument
+fs.writeFileSync("./test.txt", "Hello there");
 
-//Async
+//Async its expect callback fn
 fs.writeFile("./test.txt", "Hello there Async", (err) => {});
 
-// To read the files we use fs.readFileSync
+// To read the files we use fs.readFileSync this readFileSync is syncronous fn
+//when we use sync fn the result can be store on variable in our case we're using result as a variable
+
 const result = fs.readFileSync("./contact.txt", "utf-8");
 console.log(result);
+
+//Async readFile its expect callback function and give error and result
+fs.readFile("./contact.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log(result);
+  }
+});
 
 fs.appendFileSync("./text.txt", `${Date.now()} Hey There\n`);
 
